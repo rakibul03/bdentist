f63fcd405c566d93e288e2aabd0bb1c26602dea8
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-02-06 22:33:58 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { toast } from "react-hot-toast";
 
 function App() {
-  const notify = () => toast.success("Here is your toast.");
+
   return (
     <div className="text-xl text-gray-700 text-center font-extrabold mt-10">
       <h1>React APP</h1>
-      <button onClick={notify} className="btn btn-xs">
-        Toast
-      </button>
+
     </div>
   );
 }
 
 export default App;
+
