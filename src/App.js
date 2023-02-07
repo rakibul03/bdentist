541766a20816b7f7b44d879721dfee5d34a36080
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-02-07 23:27:57 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { toast } from "react-hot-toast";
 
 function App() {
-
   return (
     <div className="text-xl text-gray-700 text-center font-extrabold mt-10">
-      <h1>React APP</h1>
-
+      <h1>This is React APP</h1>
     </div>
   );
 }
 
 export default App;
-
