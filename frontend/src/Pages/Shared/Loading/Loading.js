e26fa61e34aea9933e96053393b7858bb4bb3b54
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-03-10 11:34:32 +0530

    Fixed errors

--- frontend/src/Pages/Shared/Loading/Loading.js
+++ frontend/src/Pages/Shared/Loading/Loading.js
@@ -6,9 +6,7 @@ const Loading = () => {
       <div
         className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
         role="status"
-      >
-        <span className="visually-hidden"></span>
-      </div>
+      ></div>
     </div>
   );
 };
