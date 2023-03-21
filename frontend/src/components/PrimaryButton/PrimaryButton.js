a34f82f09b5e9e70bb324ce7a092283369f952df
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-03-21 21:12:08 +0530

    Fixed some design

--- frontend/src/components/PrimaryButton/PrimaryButton.js
+++ frontend/src/components/PrimaryButton/PrimaryButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PrimaryButton = ({ children }) => {
   return (
-    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
+    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-gray-400">
       {children}
     </button>
   );
