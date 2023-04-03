f6fd866e0bfaafd8a32b72381ae0a1431b520936
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-04-03 21:05:24 +0530

    Handle some errors

--- backend/index.js
+++ backend/index.js
@@ -147,7 +147,7 @@ async function run() {
     });
 
     app.get("/users/admin/:email", async (req, res) => {
-      const email = req.params.email;
+      const email = req?.params.email;
       const query = { email };
       const user = await usersCollection.findOne(query);
       res.send({ isAdmin: user?.role === "admin" });
