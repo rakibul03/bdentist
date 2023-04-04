0a92b3d4dd39d96c76dc0010dbfa11168717197f
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-04-04 22:48:48 +0530

    Add plain text

--- frontend/src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ frontend/src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -8,7 +8,9 @@ const AppointmentOption = ({ appointmentOption, setTreatment }) => {
         <h2 className="text-2xl text-secondary font-bold text-center">
           {name}
         </h2>
-        <p>{slots.length > 0 ? slots[0] : "Try Another day"}</p>
+        <p>
+          {slots.length > 0 ? slots[0] : "No Booking Slot Available Today."}
+        </p>
         <p>
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
