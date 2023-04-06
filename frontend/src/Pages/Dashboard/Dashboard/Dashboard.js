041fbb144cfa9c37c2df464a7b28b3c86ec2c52d
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-04-06 22:40:42 +0530

    Added new file

--- frontend/src/Pages/Dashboard/Dashboard/Dashboard.js
+++ frontend/src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h2>Coming Soon</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Coming Soooooooon</h2>
+        </div>
+    );
 };
 
 export default Dashboard;
